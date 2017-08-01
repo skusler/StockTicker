9db6b01fba3501505913c5f6af26c2c50c7e07f6
Stephen Kusler <skusler@roadrangerusa.com>
2017-08-01 16:58:28 -0500

    First commit

---
+++ StockTicker/Program.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace StockTicker
+{
+    class Program
+    {
+        private static Timer timer;
+        private decimal lastPrice;
+        private decimal percentDifference = 0;
+        private static int MillisecondInterval = 60000;
+
+        static void Main(string[] args)
+        {
+            timer = new Timer(MillisecondInterval);
+            timer.Elapsed += HandleTimer;
+            SetTimer();
+            DisplayTickerPrice();
+
+            // Easy way to make this into a service that runs until the window is closed.
+            while (true) { }
+        }
+
+        private static void HandleTimer(Object source, ElapsedEventArgs e)
+        {
+            DisplayTickerPrice();
+        }
+
+        public static void DisplayTickerPrice()
+        {
+            // If you want more than one seperate them by , with no spaces.
+            const string tickers = "AMD,AAPL,BABA,BIDU,SQ,CAT";
+
+            string json;
+
+            using (WebClient web = new WebClient())
+            {
+                string url = $"http://finance.google.com/finance/info?client=ig&q=NASDAQ%3A{tickers}";
+                json = web.DownloadString(url);
+            }
+
+            json = json.Replace("//", "");
+
+            JArray jsonArray = JArray.Parse(json);
+
+            foreach (JToken item in jsonArray)
+            {
+                JToken ticker = item.SelectToken("t");
+                JToken price = (decimal)item.SelectToken("l");
+
+                Console.WriteLine($"{ticker.ToString().PadRight(4, ' ')} : {price}");
+            }
+
+            SetTimer();
+        }
+
+        private static void SetTimer()
+        {
+            timer.Interval = MillisecondInterval;
+            timer.Start();
+        }
+    }
+}
---
+++ StockTicker/Stock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTicker
+{
+    public class Stock
+    {
+        public string Ticker { get; set; }
+        public string Exchange { get; set; }
+        public decimal LastPrice { get; set; }
+        public DateTime LastTradeTime { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LastTradeTimeFormatted { get; set; }
+        public DateTime LastTradeDateTime { get; set; }
+        public decimal Change { get; set; }
+        public decimal ChangePercentage { get; set; }
+        public decimal AfterHoursLastPrice { get; set; }
+        public DateTime AfterHoursLastTradeTimeFormatted { get; set; }
+        public decimal Dividend { get; set; }
+        public decimal DividendYield { get; set; }
+
+    }
+}
